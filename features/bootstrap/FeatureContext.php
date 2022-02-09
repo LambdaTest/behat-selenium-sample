d1efabdcd28a4951ef1abc07168fbdfca48bbbdc
4msha <firdausia1345@gmail.com>
2022-02-09 20:05:10 +0530

    google search-todo

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -8,27 +8,34 @@ class FeatureContext extends LambdaContext {
     self::$driver->get($url);
   }
 
-  /** @When /^I search for "([^"]*)"$/ */
-  public function iSearchFor($searchText) {
-    $element = self::$driver->findElement(WebDriverBy::name("q"));
-    $element->sendKeys($searchText);
-    $element->submit();
+   /**
+     * @When I click on checkboxes
+     */
+  public function iClickOnCheckboxes() {
+    $element = self::$driver->findElement(WebDriverBy::name("li1"));
+    $element->click();
+    $element2 = self::$driver->findElement(WebDriverBy::name("li2"));
+    $element2->click();
     sleep(5);
   }
 
-  /** @Then /^I get title as "([^"]*)"$/ */
+  /**
+     * @When I add checkbox with text :arg1
+     */
+    public function iAddCheckboxWithText($arg1)
+    {
+      $inputBox = self::$driver->findElement(WebDriverBy::id("sampletodotext"));
+      $inputBox->sendKeys($arg1);
+      $addbox = self::$driver->findElement(WebDriverBy::id("addbutton"));
+      $addbox->click();
+    }
+
+
+  /** @Then /^I get checkbox text as "([^"]*)"$/ */
   public function iShouldGet($string) {
-    $title = self::$driver->getTitle();
+    $title = self::$driver->findElement(WebDriverBy::xpath("/html/body/div/div/div/ul/li[6]/span"))->getText();
     if ((string)  $string !== $title) {
       throw new Exception("Expected title: '". $string. "'' Actual is: '". $title. "'");
-    }
-  }
-
-  /** @Then /^I should see "([^"]*)"$/ */
-  public function iShouldSee($string) {
-    $source = self::$driver->getPageSource();
-    if (strpos($source, $string) === false) {
-      throw new Exception("Expected to see: '". $string. "'' Actual is: '". $source. "'");
     }
   }
 }
